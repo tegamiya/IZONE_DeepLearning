db45b8af99599894d7f4b646cc5b698c1f58a271
tegamiya <40752235+tegamiya@users.noreply.github.com>
2019-01-23 15:15:46 +0900

    Update split.py

--- split.py
+++ split.py
@@ -4,25 +4,30 @@ import sklearn
 from sklearn import model_selection
 from PIL import Image
 
-
 classes=["nako","sakura","hitomi"]
 num_classes=len(classes)
 image_resize=50
 X=[]
 Y=[]
+
 for index,classlabel in enumerate(classes):
     photo_dir="./"+classlabel
-    files=glob.glob(photo_dir+"/*.jpg")
+    files=glob.glob(photo_dir+"/*.jpg") #globでディレクトリの中の一致したものをfilesに入れる
     for i,file in enumerate(files):
         image=Image.open(file)
-        image=image.convert("RGB")
+        image=image.convert("RGB")#3色に変換
         image=image.resize((image_resize,image_resize))
         data=np.asarray(image)
         X.append(data)
-        Y.append(index)
+        Y.append(index)#Yはラベル
 
+#TensorFlowが扱いやすいデータ型にする
 X=np.array(X)
 Y=np.array(Y)
+
+#トレーニングとテストに分割
 X_train,X_test,y_train,y_test=model_selection.train_test_split(X,Y)
 xy=(X_train,X_test,y_train,y_test)
+
+#データ保存
 np.save("./izone.npy",xy)
