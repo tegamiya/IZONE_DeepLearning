05afcb0255264f79209a998b52ed30bfcd4ed502
tegamiya <40752235+tegamiya@users.noreply.github.com>
2019-01-23 15:23:24 +0900

    Update learn_aug.py

--- learn_aug.py
+++ learn_aug.py
@@ -8,14 +8,20 @@ import numpy as np
 classes=["nako","sakura","hitomi"]
 num_classes=len(classes)
 image_size=50
+
+#メイン関数定義
 def main():
+    # ファイルからデータを配列に読み込む
     X_train,X_test,y_train,y_test=np.load("./izone_aug.npy")
+    # 正規化　最大値で割って0-1に収束
     X_train=X_train.astype("float")/255
     X_test=X_test.astype("float")/255
+    # one-hot-Vector :　正解１　他は０
     y_train=np_utils.to_categorical(y_train,num_classes)
     y_test=np_utils.to_categorical(y_test,num_classes)
-
+    
     model=model_train(X_train,y_train)
+    # モデルの評価
     model_eval(model,X_test,y_test)
 
 def model_train(X,y):
@@ -32,7 +38,7 @@ def model_train(X,y):
     model.add(layers.Flatten())
     model.add(layers.Dense(512,activation="relu"))
     model.add(layers.Dense(3,activation="softmax"))
-    #コンパイル
+    #コンパイル 最適化手法の宣言 lr=ラーニングレート　loss:損失関数 正解と推定値の誤差
     model.compile(loss="binary_crossentropy",optimizer=optimizers.RMSprop(lr=1e-4),metrics=["acc"])
     #モデルの学習
     model.fit(X,y,batch_size=20,epochs=100)
@@ -40,9 +46,11 @@ def model_train(X,y):
     model.save("./izone_cnn_aug.h5")
     return model
 
+# XYにはX_test,Y_testが入っている
 def model_eval(model,X,y):
     scores=model.evaluate(X,y,verbose=1)
     print("Test acc",scores[1])
 
+#直接このプログラムが呼ばれていたらmain()を実行
 if __name__=="__main__":
     main()
